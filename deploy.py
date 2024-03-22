4ba5f1e4ef0fb2dd0e4f19f29dabd0b1aa8396d8
Saadmomin2903 <144302182+Saadmomin2903@users.noreply.github.com>
2024-03-22 23:32:03 +0530

    Update deploy.py

--- deploy.py
+++ deploy.py
@@ -6,9 +6,8 @@ from sklearn.metrics import accuracy_score
 import streamlit as st
 import pickle
 
-sc = pickle.load(open('scale1.pkl', 'rb'))
-# Load the trained model
-model = pickle.load(open('rfmodel1.pkl', 'rb'))
+sc = pickle.load(open('scale1.pkl', 'rb'))  # Load the trained scaler
+model = pickle.load(open('rfmodel1.pkl', 'rb'))  # Load the trained model
 
 st.title("Credit Card Fraud Detection Model")
 st.write("Enter the following features to check if the transaction is legitimate or fraudulent:")
@@ -28,6 +27,7 @@ if submit:
             # Convert input features to numerical values
             input_array = np.array(input_df_lst, dtype=np.float64)
             print("Input Array:", input_array)
+
             # Standardize the input features using the previously fitted scaler
             standardized_input = sc.transform(input_array.reshape(1, -1))
             print("Standardized Input:", standardized_input)
@@ -35,11 +35,21 @@ if submit:
             # Make prediction
             prediction = model.predict(standardized_input)
             print("Prediction:", prediction)
+
             # Display result
             if prediction[0] == 0:
                 st.write("Legitimate transaction")
+                st.markdown('''<iframe src="https://giphy.com/embed/k6F5QOCjp5mHLhHO7r" width="480" height="339" 
+                frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a 
+                href="https://giphy.com/gifs/paleycenter-parks-and-recreation-anniversary-paley-fest-la-2019
+                -k6F5QOCjp5mHLhHO7r">via GIPHY</a></p>''', unsafe_allow_html=True)
             else:
                 st.write("Fraudulent transaction")
+                # Embed the GIF using the provided URL
+                st.markdown('''
+                    <iframe src="https://giphy.com/embed/hgjNPEmAmpCMM" width="480" height="480" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>
+                    <p><a href="https://giphy.com/gifs/thread-discussion-august-hgjNPEmAmpCMM">via GIPHY</a></p>
+                ''', unsafe_allow_html=True)
         except ValueError as e:
             st.write("ValueError:", e)
     else:
