be91cfe573c5d918415f8e44b8e4570a612017ee
Saadmomin2903 <144302182+Saadmomin2903@users.noreply.github.com>
2024-03-23 05:48:17 +0530

    Update deploy.py

--- deploy.py
+++ deploy.py
@@ -1,8 +1,8 @@
 import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import accuracy_score
+from sklearn.ensemble import RandomForestClassifier
+
 import streamlit as st
 import pickle
 
